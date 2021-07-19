4e1924bf215d2b451bb08b4f5ec5737ff0da2b1b
jp-knj <kenji.tomita1996@gmail.com>
2021-07-19 20:28:43 +0900

    :tada: Set Router

---
+++ src/pages/NewRoom/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export const NewRoom = () => {
+  return <h2>This is NewRoom</h2>;
+};
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export const NotFound = () => {
+  return <h2>This is NotFound</h2>;
+};
