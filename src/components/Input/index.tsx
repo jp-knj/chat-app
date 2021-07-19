3677184daa4ce0ac6585600148934f21b7b84f18
jp-knj <kenji.tomita1996@gmail.com>
2021-07-19 21:17:26 +0900

    :sparkles: Add Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import React, { InputHTMLAttributes } from 'react';
+import * as Styled from './styles';
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+export const Input = (props: InputProps) => {
+  return <Styled.Input {...props} />;
+};
