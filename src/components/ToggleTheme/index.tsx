f5d0d991d3c2a2535705628220b6549f919dc4c8
jp-knj <kenji.tomita1996@gmail.com>
2021-07-23 14:30:56 +0900

    :tada: Create Theme

--- src/components/ToggleTheme/index.tsx
+++ src/components/ToggleTheme/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './styles';
-
 import { CgDarkMode } from 'react-icons/cg';
 
+import { useTheme } from './../../hooks/useTheme';
+
+import { Toast } from '../Toast';
+
 export const ToggleTheme = () => {
+  const { toggleTheme, isLight } = useTheme();
+  const [theme, setTheme] = useState('hidden');
+  const handleClick = () => {
+    setTheme('switch');
+    toggleTheme();
+    setTimeout(() => {
+      setTheme('hidden');
+    }, 2000);
+  };
   return (
     <Styled.Container>
-      <Styled.Button title="Switch Theme">
+      {theme === 'switch' && (
+        <Toast type="info"> {isLight ? 'Light' : 'Dark'}モードに変更しました!</Toast>
+      )}
+      <Styled.Button title="Switch Theme" onClick={handleClick}>
         <CgDarkMode />
       </Styled.Button>
     </Styled.Container>
