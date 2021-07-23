fad37e1a4a0c48fcfa335a635cbcf20ff5e56381
jp-knj <kenji.tomita1996@gmail.com>
2021-07-23 16:28:21 +0900

    :sparkles: Create funciton to generated room

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './../../styles/authPages';
 import { Link, useHistory } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/useAuth';
+import { database } from '../../services/firebase';
+
 import { ToggleTheme } from '../../components/ToggleTheme';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -10,10 +14,49 @@ import { Toast } from '../../components/Toast';
 import illIcon from '../../assets/images/icon_ill.svg';
 
 export const NewRoom = () => {
+  const [newRoom, setNewRoom] = useState('');
+  const [newRoomState, setNewRoomState] = useState('not-create');
   const history = useHistory();
+  const { user } = useAuth();
+
+  const handleCreateRoom = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (user) {
+      if (newRoom.trim() === '') {
+        setNewRoomState('warning');
+        setTimeout(() => {
+          setNewRoomState('not-create');
+        }, 2000);
+        return;
+      }
+
+      const roomRef = database.ref('rooms');
+
+      try {
+        const firebaseRoom = await roomRef.push({
+          title: newRoom,
+          authorId: user?.id,
+        });
+        setNewRoomState('create');
+        setTimeout(() => {
+          history.push(`/admin/rooms/${firebaseRoom.key}`);
+        }, 2100);
+      } catch {
+        setNewRoomState('error');
+      }
+    } else {
+      setNewRoomState('not-login');
+      setTimeout(() => {
+        history.push('/');
+      }, 2100);
+    }
+  };
+
   const roomList = () => {
     history.push(`/rooms`);
   };
+
   return (
     <Styled.Container>
       <Styled.Left as="aside">
@@ -27,15 +70,17 @@ export const NewRoom = () => {
           <ToggleTheme />
           <Logo height="3em" />
           <Styled.User>
-            <h2>
-              knjさん、新しい部屋を
-              <br />
-              作成してください
-            </h2>
+            <img src={user?.avatar} alt={user?.name} />
+            <h2>{user?.name}さん、新しい部屋をつくろう</h2>
           </Styled.User>
 
-          <form>
-            <Input type="text" placeholder="部屋の名前を決めよう" />
+          <form onSubmit={handleCreateRoom}>
+            <Input
+              type="text"
+              placeholder="部屋の名前を決めよう"
+              value={newRoom}
+              onChange={(event) => setNewRoom(event.target.value)}
+            />
             <Button btnType="fill" type="submit">
               新しい部屋をつくる
             </Button>
@@ -48,6 +93,14 @@ export const NewRoom = () => {
           </Styled.LinkRoom>
         </Styled.Content>
       </Styled.Right>
+      {newRoomState === 'create' && <Toast type="info">部屋をつくりました!</Toast>}
+      {newRoomState === 'warning' && (
+        <Toast type="warning">部屋の名前を入力してください！</Toast>
+      )}
+      {newRoomState === 'error' && (
+        <Toast type="error">部屋つくり、失敗しました。もう一度お願いします</Toast>
+      )}
+      {newRoomState === 'not-login' && <Toast type="error">まず、ログインしよう！</Toast>}
     </Styled.Container>
   );
 };
