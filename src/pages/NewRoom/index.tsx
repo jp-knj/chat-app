05d1af325cfc33b8e15177726e14af260840e348
jp-knj <kenji.tomita1996@gmail.com>
2021-07-23 10:36:31 +0900

    :+1: Update NewRoom Page

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import * as Styled from './../../styles/authPages';
-import { Link } from 'react-router-dom';
-
+import { Link, useHistory } from 'react-router-dom';
 import { ToggleTheme } from '../../components/ToggleTheme';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -11,12 +10,16 @@ import { Toast } from '../../components/Toast';
 import illIcon from '../../assets/images/icon_ill.svg';
 
 export const NewRoom = () => {
+  const history = useHistory();
+  const roomList = () => {
+    history.push(`/rooms`);
+  };
   return (
     <Styled.Container>
       <Styled.Left as="aside">
         <img src={illIcon} alt="" />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as suas dúvidas da sua audiência em tempo-real</p>
+        <strong>ライブQ＆A部屋をつくろう</strong>
+        <p>リアルタイムで回答しよう</p>
       </Styled.Left>
 
       <Styled.Right as="main">
@@ -32,12 +35,14 @@ export const NewRoom = () => {
           </Styled.User>
 
           <form>
-            <Input type="text" placeholder="Nome da sala" />
+            <Input type="text" placeholder="部屋の名前を決めよう" />
             <Button btnType="fill" type="submit">
               新しい部屋をつくる
             </Button>
           </form>
-          <Button btnType="outline">Verificar todas as salas</Button>
+          <Button btnType="outline" onClick={roomList}>
+            すべての部屋を確認する
+          </Button>
           <Styled.LinkRoom>
             すでにあるの部屋に<Link to="/">入る</Link>
           </Styled.LinkRoom>
