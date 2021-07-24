582386e6da893f7ddc24d8a1e168f6a9678e57f0
jp-knj <kenji.tomita1996@gmail.com>
2021-07-25 07:39:39 +0900

    :sparkles: Setup AdminRoom pages"

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import * as Styled from './styles';
+import { useHistory, useParams } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/useAuth';
+import { useRoom } from '../../hooks/useRoom';
+import { database } from '../../services/firebase';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
@@ -8,12 +14,55 @@ import { ToggleTheme } from '../../components/ToggleTheme';
 import { Card } from '../../components/Card';
 import { Question } from '../../components/Question';
 
+type RoomParams = {
+  id: string;
+};
+
 export const AdminRoom = () => {
+  const history = useHistory();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const { user, logoutWithGoogle } = useAuth();
+  const { questions, title } = useRoom(roomId);
   return (
-    <Header>
-      <RoomCode code={roomId} />
-      <Button btnType="fill">密室にする</Button>
-      <ToggleTheme />
-    </Header>
+    <Styled.Container>
+      <Header>
+        <RoomCode code={roomId} />
+        <Button btnType="fill">密室にする</Button>
+        <ToggleTheme />
+      </Header>
+      <Styled.Main>
+        <Styled.Left>
+          <Styled.Title>
+            <h1>Sala: {title}</h1>
+            <div>
+              <span>{user?.name}</span>
+              <Styled.Logout onClick={logoutWithGoogle}>deslogar</Styled.Logout>
+            </div>
+          </Styled.Title>
+        </Styled.Left>
+        <Styled.Right>
+          <Styled.Cards>
+            <Card btnStyle="primary" value={questions?.length || 0} text="質問" />
+            <Card
+              btnStyle="fill"
+              value={questions?.reduce((ac, { likeCount }) => ac + likeCount, 0) || 0}
+              text="いいね"
+            />
+            <Card
+              btnStyle="outline"
+              value={
+                questions?.reduce((ac, { isAnswer }) => {
+                  if (isAnswer) return ac + 1;
+                  return ac;
+                }, 0) || 0
+              }
+              text="Respondidas"
+            />
+          </Styled.Cards>
+        </Styled.Right>
+      </Styled.Main>
+    </Styled.Container>
   );
 };
---
+++ src/pages/AdminRoom/styles.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Main = styled.div`
+  max-width: min(80em, 70%);
+  margin: 5rem auto 3rem;
+  display: flex;
+  gap: 3rem;
+  @media screen and (max-width: 50rem) {
+    flex-direction: column-reverse;
+  }
+`;
+
+export const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 0 0 ${({ theme }) => theme.spacings.lg};
+  > h1 {
+    font-family: ${({ theme }) => theme.font.primary};
+    font-size: ${({ theme }) => theme.font.sizes.xl};
+    color: ${({ theme }) => theme.colors.textColor.primary};
+  }
+  > div {
+    display: flex;
+    color: ${({ theme }) => theme.colors.textColor.primary};
+    gap: 1rem;
+    align-items: center;
+  }
+`;
+
+export const Left = styled.div`
+  flex: 2;
+  > h1 {
+    font-family: ${({ theme }) => theme.font.primary};
+    font-size: ${({ theme }) => theme.font.sizes.lg};
+    color: ${({ theme }) => theme.colors.textColor.primary};
+  }
+  > span {
+    font-size: ${({ theme }) => theme.font.sizes.sm};
+    color: ${({ theme }) => theme.colors.white.light};
+    padding: ${({ theme }) => theme.spacings.xsm} ${({ theme }) => theme.spacings.md};
+    border-radius: 9999px;
+    font-weight: 500px;
+    background: ${({ theme }) => theme.colors.pink.medium};
+    margin-left: ${({ theme }) => theme.spacings.sm};
+  }
+`;
+
+export const Right = styled.div``;
+
+export const Cards = styled.div`
+  position: sticky;
+  top: 1em;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  @media screen and (max-width: 60rem) {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
+    gap: 1.2rem;
+  }
+`;
+
+export const Questions = styled.div``;
+
+export const Logout = styled.button`
+  background: transparent;
+  border: none;
+  color: ${({ theme }) => theme.colors.danger};
+  width: 5rem;
+  padding: 0;
+  font-size: ${({ theme }) => theme.font.sizes.xsm};
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
